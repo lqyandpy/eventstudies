1e75f374ada3b31d63139e2ea150efea0a46abf0
lqyandpy <461157910@qq.com>
2020-01-06 09:53:58 +0800

    Update remap.cumsum.R

--- R/remap.cumsum.R
+++ R/remap.cumsum.R
@@ -5,18 +5,18 @@ remap.cumsum <- function(z, is.pc = FALSE, base = 0) {
   for (i in 1:NCOL(z)) {
     tmp <- z[,i]
     if (is.pc) {
-      tmp <- tmp/100
+      tmp <- tmp/100#转换为百分制
     }
-    if(NCOL(z) == 1){
+    if(NCOL(z) == 1){#base为基数
       z <- base + cumsum(tmp)
     } else {
       z[,i] <- base + cumsum(tmp)
     }
   }
-  z
+  z#其实就是简单的累计求和
 }
 
-firstValueBase <- function(x){
+firstValueBase <- function(x){#令第一行全为0
   if(NCOL(x)==1){
    x[1] <- 0
  } else {
